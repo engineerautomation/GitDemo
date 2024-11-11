7940dd3d4d495e7d7c2963b99943969315ea8615
engineerautomation7@gmail.com <engineerautomation7@gmail.com>
2024-11-12 00:25:07 +0530

    New Class Added

---
+++ src/demo/NewClassonMasterX.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class NewClassonMasterX {
+	@Test
+	public void deleteTwitter1() {
+		System.out.println("SeleniumTest deleteTwittera");
+	}
+	
+	@Test
+	public void deleteTwitter2() {
+		System.out.println("SeleniumTest deleteTwittera");
+	}
+}
