1e53fd5a19d6197a8cea3d3c2267b4040e35b546
engineerautomation7@gmail.com <engineerautomation7@gmail.com>
2024-11-11 21:52:00 +0530

    GITX

--- src/demo/RESTAPITest.java
+++ src/demo/RESTAPITest.java
@@ -8,7 +8,11 @@ public class RESTAPITest {
 		System.out.println("postJira println GitStuff");
 		System.out.println("postJira println GitStuff2");
 		System.out.println("postJira println GitStuff3");
+<<<<<<< Updated upstream
 		System.out.println("postJira println GitStuff4");
+=======
+		System.out.println("for Conflicts");
+>>>>>>> Stashed changes
 	}
  
 	@Test
