0a8de6225ab63567421f4e7b4f90e915c373e8f2
engineerautomation7@gmail.com <engineerautomation7@gmail.com>
2024-11-11 18:04:53 +0530

    asdf

---
+++ src/demo/RESTAPITest.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.testng.annotations.*;
+
+public class RESTAPITest {
+	@Test
+	public void postJira() {
+		System.out.println("postJira");
+	}
+
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
+	}
+}
