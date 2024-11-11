ed754fee2774116c58cc1abb8b0df6ecce995fd3
engineerautomation7@gmail.com <engineerautomation7@gmail.com>
2024-11-11 22:21:12 +0530

    Commit

--- src/demo/RESTAPITest.java
+++ src/demo/RESTAPITest.java
@@ -8,11 +8,8 @@ public class RESTAPITest {
 		System.out.println("postJira println GitStuff");
 		System.out.println("postJira println GitStuff2");
 		System.out.println("postJira println GitStuff3");
-<<<<<<< Updated upstream
 		System.out.println("postJira println GitStuff4");
-=======
 		System.out.println("for Conflicts");
->>>>>>> Stashed changes
 	}
  
 	@Test
